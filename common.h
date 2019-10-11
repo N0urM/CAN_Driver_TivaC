adb41ca6c61f34e1038ba59936b4a9e17240885c
YomnaMokhtar <yomnamok@gmail.com>
2019-10-11 15:59:08 +0200

    change2

--- common.h
+++ common.h
@@ -10,35 +10,35 @@
 #define COMMON_H_
 
 
-typedef unsigned char uint8;
-typedef  char sint8;
-typedef unsigned int  uint16;
-typedef  int  sint16;
-typedef char int8;
-typedef uint8 bool_t;
+typedef unsigned char uint8_t;
+typedef  char sint8_t;
+typedef unsigned int  uint16_t;
+typedef  int  sint16_t;
+typedef char int8_t;
+typedef uint8_t bool_t;
 typedef unsigned long uint32_t;
-typedef unsigned long long uint64;
+typedef unsigned long long uint64_t;
 
 
-#define ACCESS_REG_8BIT(REG)         (*(volatile uint8*)REG)
+//#define ACCESS_REG_8BIT(REG)         (*(volatile uint8*)REG)
 
-#define ACCESS_REG_16BIT(REG)         (*(volatile uint16*)REG)
+//#define ACCESS_REG_16BIT(REG)         (*(volatile uint16*)REG)
 
-#define READ_REG_8BIT(REG)           (*(volatile uint8*)(REG))
+//#define READ_REG_8BIT(REG)           (*(volatile uint8*)(REG))
 
-#define READ_REG_16BIT(REG)           (*(volatile uint16*)(REG))
+//#define READ_REG_16BIT(REG)           (*(volatile uint16*)(REG))
 
-#define WRITE_REG_8BIT(REG,Val)      (ACCESS_REG_8BIT(REG))=(Val);
+//#define WRITE_REG_8BIT(REG,Val)      (ACCESS_REG_8BIT(REG))=(Val);
 
-#define SET_BIT(REG,BIT_NUM)		 (ACCESS_REG_8BIT(REG)|=1<<(BIT_NUM))
+#define SET_BIT(REG,BIT_NUM)		 (REG |= 1<<(BIT_NUM))
 
-#define CLEAR_BIT(REG,BIT_NUM)       (ACCESS_REG_8BIT(REG) &=(~(1<<(BIT_NUM))))
+#define CLEAR_BIT(REG,BIT_NUM)       (REG &= (~(1<<(BIT_NUM))))
 
-#define BIT_IS_SET(REG,BIT_NUM)      ((ACCESS_REG_8BIT(REG) & (1 << BIT_NUM)))
+#define BIT_IS_SET(REG,BIT_NUM)      (REG & (1 << BIT_NUM))
 
 #define BIT_IS_CLEAR(REG,BIT_NUM)    (!(BIT_IS_SET(REG,BIT_NUM)))
 
-#define TOGGLE_BIT(REG,BIT_NUM)  	 (ACCESS_REG_8BIT(REG) = ACCESS_REG_8BIT(REG) ^ (1<<BIT_NUM) )
+#define TOGGLE_BIT(REG,BIT_NUM)  	 (REG = REG ^ (1<<BIT_NUM) )
 
 
 

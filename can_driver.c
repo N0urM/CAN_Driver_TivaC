1e7fb2a227ba1a69c19b3ad62b86ebca94cc8e4d
N0urM <NourMansour@users.noreply.github.com>
2019-10-11 22:09:05 +0200

    first interrupts functions

--- can_driver.c
+++ can_driver.c
@@ -1,1 +1,50 @@
 #include "can_driver.h"
+
+void CANIntEnable (uint32_t ui32Base, uint32_t ui32IntFlags){
+    if(ui32Base == CAN0_CTL_R)
+     {   
+        CAN0_CTL_R |= ui32IntFlags;
+
+     }
+    else if (ui32Base == CAN1_CTL_R) {
+        CAN1_CTL_R |= ui32IntFlags;
+    }
+    else {
+        // Error wrong base
+    }
+    
+}
+
+void CANIntDisable (uint32_t ui32Base, uint32_t ui32IntFlags){
+    if(ui32Base == CAN0_CTL_R)
+     {   
+        CAN0_CTL_R &= (~ui32IntFlags);
+
+     }
+    else if (ui32Base == CAN1_CTL_R) {
+        CAN1_CTL_R &= (~ui32IntFlags);
+    }
+    else {
+        // Error wrong base
+    }
+}
+
+void CANIntClear (uint32_t ui32Base, uint32_t ui32IntClr){
+    if(ui32Base == CAN0_CTL_R)
+     {   
+        CAN0_INT_R &= ~ui32IntClr;
+     }
+    else if (ui32Base == CAN1_CTL_R) {
+        CAN1_INT_R &= ~ui32IntClr;
+    }
+    else {
+        // Error wrong base
+    }
+}
+
+uint32_t CANIntStatus (uint32_t ui32Base, tCANIntStsReg eIntStsReg){
+
+    
+}
+
+
--- can_driver.h
+++ can_driver.h
@@ -1,10 +1,12 @@
 #include "common.h"
+#include "tm4c123gh6pm.h"
 
 /*Const definitions*/
 
-#define CAN_INT_ERROR
-#define CAN_INT_MASTER
-#define CAN_INT_STATUS
+#define CAN_INT_ERROR  = CAN_CTL_EIE
+#define CAN_INT_MASTER = CAN_CTL_IE
+#define CAN_INT_STATUS = CAN_CTL_SIE
+#define CAN_INT_INTID_STATUS = 0x8000
 #define CAN_STATUS_BUS_OFF
 #define CAN_STATUS_EPASS
 #define CAN_STATUS_EWARN
@@ -65,12 +67,14 @@ void CANDisable (uint32_t ui32Base);
 void CANEnable (uint32_t ui32Base);
 bool_t CANErrCntrGet (uint32_t ui32Base, uint32_t pui32RxCount, uint32_t pui32TxCount);
 void CANInit (uint32_t ui32Base);
+
 void CANIntClear (uint32_t ui32Base, uint32_t ui32IntClr);
 void CANIntDisable (uint32_t ui32Base, uint32_t ui32IntFlags);
 void CANIntEnable (uint32_t ui32Base, uint32_t ui32IntFlags);
 void CANIntRegister (uint32_t ui32Base, void (pfnHandler)(void));
 uint32_t CANIntStatus (uint32_t ui32Base, tCANIntStsReg eIntStsReg);
 void CANIntUnregister (uint32_t ui32Base);
+
 void CANMessageClear (uint32_t ui32Base, uint32_t ui32ObjID);
 void CANMessageGet (uint32_t ui32Base, uint32_t ui32ObjID, tCANMsgObject psMsgObject, bool_t bClrPendingInt);
 void CANMessageSet (uint32_t ui32Base, uint32_t ui32ObjID, tCANMsgObject psMsgObject, tMsgObjType eMsgType);
